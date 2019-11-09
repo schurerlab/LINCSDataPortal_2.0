2a7e4943852af69361bd8799f01500ce377fab8c
Marcin Pilarczyk <marcin.pilarczyk@skyrise.tech>
2019-11-08 19:59:20 -0800

    add example link to signature search

--- UI-Code/src/app/components/home/SignatureSearch.js
+++ UI-Code/src/app/components/home/SignatureSearch.js
@@ -15,9 +15,12 @@ class SignatureSearch extends Component {
         this.state = {
             selectedButton:'Signature',
             cids:[],
+            // sigs:[],
             query: '',
             up:[],
             dn:[],
+            upString:[],
+            dnString:[],
             len:0,
             results:{},
             ge:{},
@@ -38,10 +41,10 @@ class SignatureSearch extends Component {
     }
     handleUpGenes(event) {
         let upGenes =[];
-        if(event.target.value.includes("\n")){
-             upGenes =event.target.value.split("\n");
-        }else if(event.target.value.includes(",")){
-            upGenes =event.target.value.split(",");
+        if(this.state.upString.includes("\n")){
+             upGenes = this.state.upString.split("\n");
+        }else if(this.state.upString.includes(",")){
+            upGenes = this.state.upString.split(",");
         }
         if(upGenes.length > 0) {
             this.setState({up: upGenes})
@@ -49,10 +52,10 @@ class SignatureSearch extends Component {
     }
     handleDownGenes(event) {
         let dnGenes =[];
-        if(event.target.value.includes("\n")){
-            dnGenes =event.target.value.split("\n");
-        }else if(event.target.value.includes(",")){
-            dnGenes =event.target.value.split(",");
+        if(this.state.dnString.includes("\n")){
+            dnGenes = this.state.dnString.split("\n");
+        }else if(this.state.dnString.includes(",")){
+            dnGenes = this.state.dnString.split(",");
         }
         if(dnGenes.length > 0){
             this.setState({dn:dnGenes})
@@ -61,6 +64,8 @@ class SignatureSearch extends Component {
     }
 
     getData(){
+        // console.log(this.state.up);
+        //   console.log(this.state.dn);
        this.setState({loading:true});
         axios.post('http://dev.ilincs.org/api/ilincsR/findConcordancesSC',
         {
@@ -86,14 +91,41 @@ class SignatureSearch extends Component {
         }
     ).then((response) => {
             this.setState({loading:false});
-            console.log(response.data.sigScores);
-            var newArray = response.data.sigScores.map(el =>{
-                return el.lincsSigID
-            });
-            console.log(newArray);
-            this.setState({cids:newArray});
+            // console.log(response.data.sigScores);
+            // var newArray = response.data.sigScores.map(el =>{
+            //     return el.lincsSigID
+            // });
+            // console.log(newArray);
+            // this.setState({cids:newArray});
+            this.setState({cids:response.data.sigScores});
 
         })
+    }
+
+    setExampleGenes(){
+        // console.log(e);
+        
+        // e.preventDefault();
+        console.log("setExampleGenes function")
+        // this.handleUpGenes("1,2,3");
+        // this.handleDownGenes("a,b,c");
+        let upGenes,dnGenes;
+        upGenes = "ESR1,GREB1,LRRC6,GPR143,CCDC170,SCUBE2,ABAT,ST6GALNAC2,CACNA2D2,RHOH,TFF3,IQSEC1,CLSTN2,REPS2,CACNA1H,MRFAP1L1,ATP7B,CA5B,ARMT1,PRLR,CACNG4,XBP1,CRYL1,SLC9A2,ANXA9,ZMAT4,KIAA1324,KIAA1467,SULT2B1,BCAS1,MED13L,SEPT8,ADD1,ZFYVE21,BCAS3,CYFIP2,CHRD,COX6C,TOB1,MREG,SIAH2,GHR,GATA3,KDM2A,COLQ,HTT,SCGN,CA12,KIF16B,PVALB,SOX2,SIRT4,CBFA2T3,BEX4,GPD1L,SPDEF,TGFB3,KIAA0513,ASTN2,RAB5B,SHROOM2,SUOX,CYP46A1,CLU,EFHD1,ASAH1,SIDT1,SLC22A5,ALDH3B2,RHOB,KDM5B,MAGED2,SLC7A8,GADD45G,NCAM2,ETNK2,PTGER3,MCCC2,SUPT4H1,CTPS2,GRIK4,SLC1A2,TCTN1,CERS4,TTC39A,ELOVL2,IGSF3,C11orf80,SLC35E3,ALDH6A1,ADIRF,CPT1A,KIAA0100,ARSD,UNC13B,LCMT1,SNF8,HOXC4,IGFBP2,ZNF652";
+        dnGenes = "SLC35G2,THEMIS2,MED17,CDKL1,CCDC88A,STAC,IFI16,MICAL2,GSTP1,SNRNP40,GNAI1,HOXA7,PRKCDBP,BATF3,ETV4,YEATS2,SNAI2,RASAL2,USB1,ISG15,GART,AKR1C1,BIRC3,SKI,CSRP2,TGFBI,TRIP10,PTX3,FST,MMP14,COLGALT1,HPSE,C1R,FXYD5,MICALL1,PSAT1,CERK,GLIPR1,CAPN2,SPTBN1,GPX1,NASP,PRSS12,SPRY2,EPHA2,DSE,TOMM22,MIEF1,PTK7,BCL11A,IGFBP7,MAP1B,COL4A1,FOSL1,TAP2,CSNK1E,SPCS3,POLR2F,JOSD1,BTG3,MEIS2,PHC2,CAV1,NRP2,MAP7D1,SERPINB7,LUZP1,MDFIC,TINAGL1,ADAM19,EGFR,CXCL3,HDAC9,CAV2,ADCY7,SERPINB8,SLC7A1,GPR3,STX11,SRSF4,CASP4,PLAUR,NABP1,ACSL5,BIN1,FSCN1,MSN,TNFAIP3,PIK3CD,YBX1,ETS1,AGPAT4,PDGFC,CXCL8,CXCL2,COL4A2,HRH1,GADD45A,SDHAF3,CTSC";
+        // console.log(this.state);
+        
+        this.setState({
+            upString: upGenes,
+            dnString: dnGenes,
+            up: upGenes.split(","),
+            dn: dnGenes.split(",")
+          });
+        
+        //   console.log(this.state.upString);
+        //   console.log(this.state.dnString);
+          
+        // this.handleUpGenes();
+        // this.handleDownGenes();
     }
 
     render() {
@@ -101,15 +133,19 @@ class SignatureSearch extends Component {
             {this.state.loading == false && this.state.cids.length == 0 ?  <form>
                 <div className="form-row">
                     <div className="col">
-                        <textarea name="styled-textarea" id="styled" type="text" value={this.state.value} onChange={this.handleUpGenes} placeholder="Enter UP Genes either seperated by coma or new line "/>
+                        <textarea name="styled-textarea" id="styled" type="text" value={this.state.upString} onChange={this.handleUpGenes} placeholder="Enter UP Genes either seperated by coma or new line "/>
                     </div>
                     <div className="col text-center " style={{marginTop:"20em"}}>
+                    <a href="#" onClick={() => {this.setExampleGenes()} }>
+                        Example
+                    </a>
+                    <br />
                     <Button className="btn btn-primary  mb-2"  onClick={() => {this.getData()} }>
                         Submit
                     </Button>
                         </div>
                     <div className="col">
-                        <textarea name="styled-textarea" id="styled" type="text" value={this.state.value} onChange={this.handleDownGenes}  placeholder="Enter Down Genes either seperated by coma or new line "/>
+                        <textarea name="styled-textarea" id="styled" type="text" value={this.state.dnString} onChange={this.handleDownGenes}  placeholder="Enter Down Genes either seperated by coma or new line "/>
                     </div>
                 </div>
             </form> : "" }
