ce564fdd658742f843445543c00cadb8e9656628
Marcin Pilarczyk <marcin.pilarczyk@skyrise.tech>
2019-12-10 14:31:59 -0800

    added Router basename to Main.js

--- UI-Code/src/app/layouts/Main.js
+++ UI-Code/src/app/layouts/Main.js
@@ -33,6 +33,7 @@ class Main extends React.Component {
     render() {
         return (
             <BrowserRouter>
+                <Router basename="/signatures-test/"></Router>
                 <div>
                     <Navbar />
 
