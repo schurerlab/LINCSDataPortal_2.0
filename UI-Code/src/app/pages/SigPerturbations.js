31f0755f199d7db6f1807b28141e4de219c33486
Akoleti <akoleti@miami.edu>
2020-06-09 10:09:44 -0400

    added knockdown to sigperturbations page

--- UI-Code/src/app/pages/SigPerturbations.js
+++ UI-Code/src/app/pages/SigPerturbations.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux"
-
+import {Grid, Row, Col, Accordion, Panel, ListGroup, ListGroupItem,ButtonToolbar,FormGroup,InputGroup,ToggleButtonGroup,ToggleButton,ButtonGroup, Button,Table,Well,FormControl,Modal,Tabs,Tab} from 'react-bootstrap';
 import Perturbations from "../components/perturbations/Perturbations";
+import Knockdown from "../components/perturbations/Knockdown";
 import PerturbationDetail from "../components/perturbations/PerturbationDetail"
 // import PerturbationsSearch from "../components/perturbations/";
 // import PerturbationsDescription from "../components/perturbations/PerturbationsDescription";
@@ -10,10 +11,45 @@ import PerturbationDetail from "../components/perturbations/PerturbationDetail"
 
 export default class SigPerturbations extends React.Component{
 
+    constructor(props) {
+        super();
+
+        this.state = {
+            selected: 0,
+            selectedButton:'Small molecules',
+            currentPage:1,
+            todosPerPage:10,
+        }
+
+    }
+    buttonSelected(key){
+        if(this.state.selectedButton!=key){
+            this.setState({selectedButton: key })
+            console.log(this.state.selectedButton);
+        }
+
+
+    }
+
     render() {
       return (
-          <div >
-              <Perturbations></Perturbations>
+          <div>
+          <ButtonToolbar >
+          <ButtonGroup >
+            <Button className={this.state.selectedButton === 'Small molecules' ? "ms_active" : "btn-default"}  onClick={() => {this.buttonSelected('Small molecules')} }>
+        <br/>
+        <span style={{fontSize:"1.5em"}}>Small molecules </span>
+        </Button>
+        <Button  className={this.state.selectedButton === 'Knock down' ? "ms_active" : " btn-default"}  onClick={() => {this.buttonSelected('Knock down')}}>
+        <br/>
+        <span style={{fontSize:"1.5em"}}> Knock down </span>
+        </Button>
+
+        </ButtonGroup>
+        </ButtonToolbar>
+
+        {this.state.selectedButton === 'Small molecules' ? <Perturbations></Perturbations>  :""}
+              {this.state.selectedButton === 'Knock down' ? <Knockdown></Knockdown>  :""}
 
           </div>
 
