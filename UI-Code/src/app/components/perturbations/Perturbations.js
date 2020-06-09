427191807f0aa181705d6f86e1e0d1a94d367617
Akoleti <akoleti@miami.edu>
2020-06-09 10:55:23 -0400

    updated url

--- UI-Code/src/app/components/perturbations/Perturbations.js
+++ UI-Code/src/app/components/perturbations/Perturbations.js
@@ -321,7 +321,7 @@ class Perturbations extends React.Component {
         }else {
             axios.request({
                 method: 'get',
-                url: 'http://dev3.ccs.miami.edu:8080/sigc-api-test/small-molecule/fetch?limit=20&page='+this.state.page+'&returnSignatures=false'
+                url: '/sigc-api-test/small-molecule/fetch?limit=20&page='+this.state.page+'&returnSignatures=false'
             }).then((response) => {
 
 
