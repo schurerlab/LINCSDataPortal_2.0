04ccf7f5aa804a9ddfa8732b9f01336ccc458e70
Akoleti <akoleti@miami.edu>
2020-08-14 17:33:19 -0400

    Added Sentry

--- UI-Code/src/app/index.js
+++ UI-Code/src/app/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-
+import * as Sentry from '@sentry/react';
 import { Provider } from "react-redux";
 
 import Main from "./layouts/Main"
@@ -9,6 +9,8 @@ import reduxStore from "./store";
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Sentry.init({dsn: "https://5d3a06587e2541f7aafbc862ddcc85e7@o433314.ingest.sentry.io/5391269"});
 
 render(
     <Provider store={reduxStore}>
